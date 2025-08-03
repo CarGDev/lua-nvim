6919892952f65d3dd81ba293f3d27be86b5146d2
Carlos <ingecarlos.gutierrez@gmail.com>
2025-08-03 11:03:18 -0400

    adding new theme

--- lua/cargdev/plugins/colorscheme.lua
+++ lua/cargdev/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
-  'CarGDev/cargdev-cyberpunk',
+  "CarGDev/cargdev-cyberpunk",
+  --[[   dir = "/Users/carlos/Documents/SSD_Documents/projects/cargdevschemecolor.nvim", ]]
   config = function()
-    require('cargdev-cyberpunk').setup()
+    require("cargdev-cyberpunk").setup()
   end,
   priority = 1000,
   lazy = false,
