ca3c265533f785ce37a2c5722e3b20e8d087ddbd
Carlos Gutierrez <cargdev@gmail.com>
2026-03-31 10:58:55 -0400

    adding logic to choose the repo if the local repo does not exist

--- lua/cargdev/plugins/ideaMap.lua
+++ lua/cargdev/plugins/ideaMap.lua
@@ -18,13 +18,19 @@
 -- Get local config (loaded in core/init.lua)
 local local_cfg = vim.g.cargdev_local or {}
 
--- Skip plugin if IDEA_DIR is not configured
-if not local_cfg.IDEA_DIR then
-  return {}
+local repo = "CarGDev/ideadrop.nvim"
+local localDir = nil
+
+-- If requested to run ideadrop locally and a path is provided, use it.
+if local_cfg.RUN_IDEA_LOCAL and local_cfg.CODE_TYPER_DIR and #local_cfg.CODE_TYPER_DIR > 0 then
+  localDir = local_cfg.CODE_TYPER_DIR
 end
 
 return {
-  "CarGDev/ideadrop.nvim",
+  -- Codetyper.nvim - AI-powered coding partner
+  -- Local development version
+  repo,
+  dir = localDir,
   name = "ideaDrop",
   dependencies = {
     "nvim-tree/nvim-tree.lua",
@@ -34,7 +40,7 @@ return {
     require("ideaDrop").setup({
       idea_dir = vim.g.cargdev_local.IDEA_DIR,
     })
-    
+
     -- Set up convenient keymaps for ideaDrop
     vim.keymap.set("n", "<leader>id", ":IdeaRight<CR>", { desc = "Open today's idea in right buffer" })
     vim.keymap.set("n", "<leader>in", ":IdeaRight ", { desc = "Open named idea in right buffer" })
@@ -42,7 +48,7 @@ return {
     vim.keymap.set("n", "<leader>is", ":IdeaSearch ", { desc = "Search ideas" })
     vim.keymap.set("n", "<leader>ig", ":IdeaTags<CR>", { desc = "Browse tags" })
     vim.keymap.set("n", "<leader>if", ":Idea<CR>", { desc = "Open today's idea in float" })
-    
+
     -- Optional: Override the default :Idea command to use right buffer instead of float
     -- Uncomment the line below if you want :Idea to always use the right buffer
     -- vim.api.nvim_create_user_command("Idea", function(opts) vim.cmd("IdeaRight " .. (opts.args or "")) end, { nargs = "?" })
