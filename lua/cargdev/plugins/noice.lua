782168b9cd5b48f1e065bda6140ed82c8f6f038c
Carlos Gutierrez <cargdev@gmail.com>
2026-07-01 20:11:08 -0400

    Adding unsupervised fraud learning project

--- lua/cargdev/plugins/noice.lua
+++ lua/cargdev/plugins/noice.lua
@@ -13,6 +13,15 @@ return {
     cmdline = {
       enabled = true,
       view = "cmdline_popup",
+      format = {
+        cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+        filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+        input = { view = "cmdline_input", icon = "󰥻 " },
+      },
     },
     views = {
       cmdline_popup = {
@@ -27,7 +36,14 @@ return {
         border = { style = "rounded" },
       },
     },
-    messages = { enabled = false },
+    messages = {
+      enabled = true, -- enables the Noice messages UI
+      view = "notify", -- default view for messages
+      view_error = "notify", -- view for errors
+      view_warn = "notify", -- view for warnings
+      view_history = "messages", -- view for :messages
+      view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+    },
     popupmenu = { enabled = false },
     notify = { enabled = false },
     lsp = {
